752332463f11a9825946b760ad965d965afe4505
Geunyoung Park <gypark@shou.kr>
2017-04-14 16:00:01 +0900

    Post entity

---
+++ src/main/java/com/millky/blog/domain/model/entity/Post.java
@@ -0,0 +1,77 @@
+package com.millky.blog.domain.model.entity;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue
+    int id;
+    String subject;
+    
+    @Column(length = 100000000)
+    String content;
+    
+    Date regDate;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the subject
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subject the subject to set
+     */
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the regDate
+     */
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    /**
+     * @param regDate the regDate to set
+     */
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+
+}
