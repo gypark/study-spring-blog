ce287cf6e070b4fec9357af041f7c51f1e024cd3
Geunyoung Park <gypark@shou.kr>
2017-04-14 15:59:12 +0900

    PostDao

--- src/main/java/com/millky/blog/infrastructure/dao/HelloDao.java
+++ src/main/java/com/millky/blog/infrastructure/dao/HelloDao.java
@@ -1,7 +1,6 @@
 package com.millky.blog.infrastructure.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.millky.blog.domain.model.entity.Hello;
 
 public interface HelloDao extends JpaRepository<Hello, Integer> {
